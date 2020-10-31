feb511894bf2d1343aea55206c58e2f535e93341
User <aslan.rustemov777@gmail.com>
2020-11-01 02:31:52 +0400

    Added methods

--- app/views.py
+++ app/views.py
@@ -1,8 +1,33 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from . import serializers
 
 
 class HelloApiView(APIView):
+    serializer_class = serializers.HelloViewSerializer
+
     def get(self, request, format=None):
         context = ['Hello']
         return Response({'message': context})
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        # Update full object
+        return Response({'message': 'Put'})
+
+    def patch(self, request, pk=None):
+        # Update part of object
+        return Response({'message': 'Patch'})
+
+    def delete(self, request, pk=None):
+
+        return Response({'message': 'Delete'})
