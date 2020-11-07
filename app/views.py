8b582ba409db623de92371bb479fdec58197fd3a
User <aslan.rustemov777@gmail.com>
2020-11-08 03:28:19 +0400

    Added login API

--- app/views.py
+++ app/views.py
@@ -2,11 +2,10 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, viewsets
 from rest_framework.authentication import TokenAuthentication
-
-from app import models
-from app import serializers
-from app import permissions
 from rest_framework.filters import SearchFilter
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+from app import models,serializers,permissions
 
 
 class HelloApiView(APIView):
@@ -77,3 +76,6 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (SearchFilter,)
     search_fields = ('name', 'email',)
+
+class UserLoginApiView(ObtainAuthToken):
+    renderer_classes=api_settings.DEFAULT_RENDERER_CLASSES
